218503f3d3850c6903f6ed9a3c2c33f70f28b9e3
HenryMV9 <victorenoche5@gmail.com>
2025-11-06 11:43:00 +0000

    Updated config.js

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    SUPABASE_URL: 'https://hzgmmvurvtfgmzoaqurm.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6Z21tdnVydnRmZ216b2FxdXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDMyMDMsImV4cCI6MjA3NjgxOTIwM30.J3wB8WVrjCBlzLuycTiZ66oD9eY_Z66Pevi9ANqITxI',
+    SUPABASE_URL: 'https://rzeymsrusybhxfkmapas.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6ZXltc3J1c3liaHhma21hcGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMTA0NTIsImV4cCI6MjA3Nzc4NjQ1Mn0.kjHvcNGcf0ZXi7rUtHIejVLvV11H_rjCWhEWKKn-ekA',
     PAYSTACK_PUBLIC_KEY: 'pk_live_765f4190f3dfabb8c6ca745fc9d4bec8fb0b320c'
 };
