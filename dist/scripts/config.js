39f1eaf57f2ce9a87dd5c214d8ce71cf9ceb39a5
HenryMV9 <victorenoche5@gmail.com>
2026-03-01 10:21:46 -0800

    Deleted contact.html

--- dist/scripts/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    SUPABASE_URL: 'https://rzeymsrusybhxfkmapas.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6ZXltc3J1c3liaHhma21hcGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMTA0NTIsImV4cCI6MjA3Nzc4NjQ1Mn0.kjHvcNGcf0ZXi7rUtHIejVLvV11H_rjCWhEWKKn-ekA',
-    PAYSTACK_PUBLIC_KEY: 'pk_live_765f4190f3dfabb8c6ca745fc9d4bec8fb0b320c'
-};
